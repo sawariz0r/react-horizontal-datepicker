df1c490895450c156dd559bbd1ac990a23cd6ea1
Oscar Nilsson <oscar.nilsson@qte.se>
2021-01-24 01:49:24 +0100

    updated

--- dist/DatePicker.js
+++ dist/DatePicker.js
@@ -9,11 +9,11 @@ export default function DatePicker({
   color,
   labelFormat
 }) {
-  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(selectDate || new Date());
   const firstSection = {
     marginLeft: '40px'
   };
-  const startDate = new Date();
+  const startDate = selectDate || new Date();
   const lastDate = addDays(startDate, endDate || 90);
   const primaryColor = color || 'rgb(54, 105, 238)';
   const selectedStyle = {
@@ -107,6 +107,8 @@ export default function DatePicker({
     if (getSelectedDay) {
       if (selectDate) {
         getSelectedDay(selectDate);
+      } else {
+        getSelectedDay(startDate);
       }
     }
   }, []);
